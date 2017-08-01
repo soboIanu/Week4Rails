c3b3683f7d5e7fd55c42a6e8b9780474180a8921
sobolanu <botezatu.richard@gmail.com>
2017-08-01 13:54:32 +0200

    Finished user edit, update, index, destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def show
     @user = User.find(params[:id])
@@ -37,6 +42,7 @@ class UsersController < ApplicationController
 
   def logged_in_user
   unless logged_in?
+    store_location
     flash[:danger] = "Please log in."
     redirect_to login_url
   end
@@ -47,10 +53,21 @@ def correct_user
       redirect_to(root_url) unless current_user?(@user)
 end
 
+def destroy
+  User.find(params[:id]).destroy
+  flash[:success] = "User deleted"
+  redirect_to users_url
+end
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+
 end
